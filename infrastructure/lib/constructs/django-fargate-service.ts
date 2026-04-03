65256330bf8d2f403385ecbf3010c52d68ee59da
Bobby R. Stephens III <60822197+Bobbyiii21@users.noreply.github.com>
2026-04-02 21:31:19 -0400

    feat: app layout, Docker, CDK ECS stack
    Move Django project under app/. Add Dockerfile and .dockerignore (build from repo root).
    Add AWS CDK TypeScript stack for Fargate + ALB. Narrow /lib/ gitignore so infrastructure/lib is tracked.

--- infrastructure/lib/constructs/django-fargate-service.ts
+++ infrastructure/lib/constructs/django-fargate-service.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
@@ -14,6 +15,8 @@ export interface DjangoFargateServiceProps {
   readonly awsRegion: string;
   readonly subnets: string[];
   readonly serviceSecurityGroupId: string;
+  readonly targetGroup: elbv2.CfnTargetGroup;
+  readonly httpListener: elbv2.CfnListener;
   readonly containerName: string;
   readonly containerPort: number;
   /** Extra container env vars (e.g. Bedrock IDs). */
@@ -109,7 +112,15 @@ export class DjangoFargateService extends Construct {
           securityGroups: [props.serviceSecurityGroupId],
         },
       },
+      loadBalancers: [
+        {
+          containerName: props.containerName,
+          containerPort: props.containerPort,
+          targetGroupArn: props.targetGroup.ref,
+        },
+      ],
     });
     this.service.overrideLogicalId('Service');
+    this.service.addDependency(props.httpListener);
   }
 }
